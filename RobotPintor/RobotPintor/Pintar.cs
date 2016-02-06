1aead1ec1ef053fa2eca2eb5cb48ce035588e749
Raksus <irda22r@gmail.com>
2016-02-06 16:27:17 +0100

    Implementado la pintada

--- RobotPintor/RobotPintor/Pintar.cs
+++ RobotPintor/RobotPintor/Pintar.cs
@@ -6,6 +6,9 @@ namespace RobotPintor {
 
         private string file;
         private char[,] matrizInicial;
+        private char[,] matrizResultado;
+        private int ancho;
+        private int alto;
 
         public Pintar(string file) {
             this.file = file;
@@ -18,12 +21,12 @@ namespace RobotPintor {
             string dimension = toPaint.ReadLine();
             string[] dimensionSplit = dimension.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
 
-            int alto, ancho;
-            alto = int.Parse(dimensionSplit[0]);
-            ancho = int.Parse(dimensionSplit[1]);
+            this.alto = int.Parse(dimensionSplit[0]);
+            this.ancho = int.Parse(dimensionSplit[1]);
 
             Console.WriteLine("alto: " + alto + ", ancho: " + ancho);
             matrizInicial = new char[alto, ancho];
+            matrizResultado = new char[alto, ancho];
             for (int i = 0; i < alto; i++) {
                 string tempLine = toPaint.ReadLine();
                 for (int j = 0; j < ancho; j++) {
@@ -41,21 +44,59 @@ namespace RobotPintor {
 
         public void StartPaint() {
             Console.WriteLine("Pintando");
-
+            PintarCuadrado(new Punto(3, 3), 2);
+            PintarLinea(new Punto(8, 2), new Punto(8, 6));
+            PintarLinea(new Punto(8, 7), new Punto(10, 7));
+            PrintMatriz(matrizResultado, this.ancho, this.alto);
         }
 
         private void PintarCuadrado(Punto centro, int radio) {
-
+            matrizResultado[centro.x, centro.y] = char.Parse("#");
+            if (radio > 0) {
+                for (int i = -radio; i < radio + 1; i++) {
+                    for (int j = -radio; j < radio + 1; j++) {
+                        matrizResultado[centro.x + i, centro.y + j] = char.Parse("#");
+                    }
+                }
+            }
         }
 
         private void PintarLinea(Punto origen, Punto destino) {
             if (origen.x == destino.x) {
+                if (destino.y - origen.y < 0) {
+                    for (int i = 0; i < origen.y - destino.y; i++) {
+                        matrizResultado[origen.x, origen.y + i] = char.Parse("#");
+                    }
+                } else {
+                    for (int i = 0; i < destino.y - origen.y; i++) {
+                        matrizResultado[origen.x, destino.y + i] = char.Parse("#");
+                    }
+                }
                 //Linea Horizontal
             } else if (origen.y == destino.y) {
+                if (destino.x - origen.x < 0) {
+                    for (int i = 0; i < origen.x - destino.x; i++) {
+                        matrizResultado[origen.x + i, origen.y] = char.Parse("#");
+                    }
+                } else {
+                    for (int i = 0; i < destino.x - origen.x; i++) {
+                        matrizResultado[destino.x + i, origen.y] = char.Parse("#");
+                    }
+                }
                 //Linea Vertical
             } else {
                 Console.WriteLine("Unspected Value");
             }
+        }
+
+        private void PrintMatriz(char[,] matriz, int ancho, int alto) {
+            for (int i = 0; i < alto; i++) {
+                Console.WriteLine("");
+                for (int j = 0; j < ancho; j++) {
+                    Console.Write(matriz[i, j]);
+                }
+            }
+
         }
     }
 }
